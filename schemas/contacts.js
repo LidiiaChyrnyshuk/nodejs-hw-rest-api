4926742aebbe2ecaa1216adb90591edefe0d793a
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-07-18 23:22:14 +0300

    fix schema

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -2,19 +2,15 @@ const Joi = require("joi");
 
 const addSchema = Joi.object({
 	name: Joi.string().required().messages({
-		"any.required": 'missing required "name" field',
+		"any.required": `missing required name field`,
 	}),
 	email: Joi.string().email().required().messages({
-		"any.requred": 'missing required "email" field',
+		"any.required": `missing required email field`,
 	}),
 	phone: Joi.string().required().messages({
-		"any.requred": 'missing required "phone" field',
+		"any.required": `missing required phone field`,
 	}),
-})
-	.required()
-	.messages({
-		"any.requred": "missing fields",
-	});
+});
 
 module.exports = {
 	addSchema,
