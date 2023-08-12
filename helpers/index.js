43857572cba5d34551969f5b4efd6f934d6f39ef
Lidiia Chyrnyshuk <l.mandych@gmail.com>
2023-08-12 23:27:47 +0300

    add verification and Docker

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,11 @@
 const HttpError = require("./HttpError");
 const ctrlWrapper = require("./ctrlWrapper");
 const resizeAvatarImg = require("./resizeAvatarImg");
+const sendEmail = require("./sendEmail");
 
 module.exports = {
 	HttpError,
 	ctrlWrapper,
 	resizeAvatarImg,
+	sendEmail,
 };
